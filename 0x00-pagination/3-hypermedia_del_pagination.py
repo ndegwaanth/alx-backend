fe6a7d485dfb94fb7d9b5338298a907ba5496245
Anthony <ndegwaanthony300@gmail.com>
2024-07-22 23:36:09 +0300

    3-hypermedia_del_pagination.py

--- 0x00-pagination/3-hypermedia_del_pagination.py
+++ 0x00-pagination/3-hypermedia_del_pagination.py
@@ -38,7 +38,7 @@ class Server:
             }
         return self.__indexed_dataset
 
-    def get_hyper_index(self, index: Optional[int] = None, page_size: int = 10) -> Dict:
+    def get_hyper_index(self, index: int = None, page_size: int = 10) -> Dict:
         """Get a page of the dataset using the index for pagination
         """
         # Assert that index is valid
