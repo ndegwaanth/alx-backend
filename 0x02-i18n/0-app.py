79f6da6d0d4599685ef5ab9445791796749d64e6
Anthony <ndegwaanthony300@gmail.com>
2024-07-31 00:47:17 +0300

    basic flask application

--- 0x02-i18n/0-app.py
+++ 0x02-i18n/0-app.py
@@ -7,11 +7,11 @@ app = Flask(__name__)
 
 
 @app.route('/')
-def show():
+def index():
     """This function render the index.html file"""
     return render_template('index.html')
 
 
 if __name__ == '__main__':
-
+    """This is the entry point of the application"""
     app.run(debug=True)
